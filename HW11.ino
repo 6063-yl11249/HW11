468830906ac3475d3f4781dd061a8524dbec4a3e
Yuanqing <yl11249@nyu.edu>
2023-11-22 22:37:02 -0500

    Password Machine

--- HW11.ino
+++ HW11.ino
@@ -1,9 +1,69 @@
+#include "Clicky.h"
+
+
+int button0 = 0;
+int button1 = 1;
+bool GreenState = false;
+bool RedState = false;
+int cCount = 0;
+int threshold  = 4000;
+
+int button0_State = 0;
+int button1_State = 0;
+
+Clicky Button_0;
+Clicky Button_1;
+float potentiometer;
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Button_0.setup(2);
+  Button_1.setup(3);
+
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  int a0v = analogRead(A0);
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  bool is_button0_clicked = Button_0.wasClicked();
+  bool is_button1_clicked = Button_1.wasClicked();
+  int a0 = analogRead(A0);
+
+  if (is_button0_clicked && button0_State == 0 && button1_State == 0) {
+    button0_State = 1;
+    cCount++;
+  }else if (is_button1_clicked && button0_State == 1 && button1_State == 0){
+    button1_State = 1;
+    cCount++;
+  }else if (is_button1_clicked && button1_State == 1 && button0_State == 1){
+    GreenState = true;
+    cCount++;
+  }else if(is_button0_clicked || is_button1_clicked) {
+    cCount++;
+  }
+  
+  if (GreenState == false && cCount == 3){
+    RedState = true;
+  }
+  
+  if (GreenState && a0 > 4000){
+    digitalWrite(5, HIGH);
+    delay(500);
+    digitalWrite(5, LOW);
+    delay(500);
+  }else if(RedState){
+    digitalWrite(6, HIGH);
+    delay(500);
+    digitalWrite(6, LOW);
+    delay(500);
+  }
+
+  Serial.println("Button_0_State" + String(button0_State)+ "Button_1_State?" + String(button1_State) + "cCount" + String(cCount) + String(a0));
+
+
+  delay(10);
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # HW11
- 
+ *Drawing of the Finite State Machine.*  
+
+ *Description of the correct steps.*  
+1. Press button 0.   
+2. Press button 1 twice.  
+3. Turn potentiometer all the way to the right.  
+
+ *A schematic drawing of the circuit.*   
+
